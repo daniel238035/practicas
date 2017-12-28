afa8feebebed4c03fb886d2133852b55d7ec8d0c
DANIELCOREI7\Daniel <daniel238035@hotmail.com>
2017-12-28 17:17:44 -0500

    Inicio del Proyecto

---
+++ movilidad.php
@@ -0,0 +1,194 @@
+<?php include("header.php"); ?>
+
+  <div class="">
+        <div class="row portada-sub portada-4 ">
+          <div class="portada-principal-oscuro flex seccion-2">
+              <div class="row">
+                <div class="col s12 l5 offset-l2">
+                  <h2 class="white-text">MOVILIDAD ESTUDIANTIL</h2>
+                </div>
+              </div>
+          </div>
+        </div>
+
+        <div class="row seccion">
+          <div class="col s12 offset-l1 l7">
+
+            <div class="row">
+              <h4 class="text-negrita">Beca Presidente de la Republica</h4>
+            </div>
+
+            <div class="row" style="padding-bottom: 5px">
+              <div class="col s6">
+                <div class="row" style="margin: 8px 0px!important;" >
+                    <div class="col s4 flex-row-start text-negrita">
+                      <i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Institución">place</i>
+                      Institución:
+                    </div>
+                    <div class="col s8">
+                      Universidad Nacional Pedro Ruiz Gallo
+                    </div>
+                </div>
+              </div>
+              <div class="col s6">
+                <div class="row" style="margin: 8px 0px!important;">
+                    <div class="col s4 flex-row-start text-negrita">
+                      <i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Inicio">event</i>
+                      Inicio:
+                    </div>
+                    <div class="col s8">
+                      18 de agosto del 2017
+                    </div>
+                </div>
+                <div class="row" style="margin: 8px 0px!important;">
+                    <div class="col s4 flex-row-start text-negrita">
+                      <i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Duración">date_range</i>
+                      Duración:
+                    </div>
+                    <div class="col s8">
+                      3 años
+                    </div>
+                </div>
+              </div>
+              <div class="col s12">
+                <div class="row" style="margin: 15px 0px!important;">
+        					<img src="images/portadas/portada3.jpg" class="responsive-img z-depth-1 imagen-evento" style="width: 100%;!important; height:450px!important;">
+        				</div>
+              </div>
+
+              <div class="col s12 " id="presentacion" style="margin:15px 0px!important;">
+                <h4 class="color-secundario" >PRESENTACIÓN</h4>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. In illum cumque accusamus, cum possimus porro quas necessitatibus aspernatur voluptatem sunt, tempore ipsam animi dolorum velit iure assumenda ut ratione voluptatum non? Eum nisi quas nam illum ea, consectetur voluptatem commodi doloremque neque harum, obcaecati vero sapiente magni nulla repudiandae eos facere pariatur, cum eius vel ut vitae alias. Vero ipsum, dolore provident accusamus sequi maiores incidunt commodi dignissimos minima laudantium, aut, iusto architecto suscipit doloribus cupiditate culpa, ratione similique fugit molestiae tenetur. Fugit blanditiis facere dolorum, beatae officia dolore quae. Quam similique at sunt repellendus sit voluptas beatae aperiam doloremque.
+              </div>
+
+              <div class="col s12 " id="beneficios" style="margin:15px 0px!important;">
+                <h4 class="color-secundario">BENEFICIOS</h4>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque in debitis pariatur sequi veniam corporis officia ducimus officiis, possimus, maxime beatae assumenda quae tempore a consequatur libero ullam dignissimos quam, quia vel modi ex. Dicta nisi unde quasi id voluptatibus molestiae mollitia quidem et optio, provident veniam illum vitae soluta atque quaerat, est in possimus repellat quis pariatur, a dolore! Dolorem saepe voluptatibus debitis nostrum vero! Id quod labore iste dicta debitis porro dolorem quae eligendi, nam blanditiis! Quod, architecto vel aut minima, quas officiis itaque cupiditate asperiores facilis aperiam illo aliquid labore animi? Laboriosam ab quod officia necessitatibus porro!
+              </div>
+
+              <div class="col s12 " id="instituciones_elegibles" style="margin:15px 0px!important;">
+                <h4 class="color-secundario">IES Elegibles</h4>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque in debitis pariatur sequi veniam corporis officia ducimus officiis, possimus, maxime beatae assumenda quae tempore a consequatur libero ullam dignissimos quam, quia vel modi ex. Dicta nisi unde quasi id voluptatibus molestiae mollitia quidem et optio, provident veniam illum vitae soluta atque quaerat, est in possimus repellat quis pariatur, a dolore! Dolorem saepe voluptatibus debitis nostrum vero! Id quod labore iste dicta debitis porro dolorem quae eligendi, nam blanditiis! Quod, architecto vel aut minima, quas officiis itaque cupiditate asperiores facilis aperiam illo aliquid labore animi? Laboriosam ab quod officia necessitatibus porro!
+              </div>
+
+            </div>
+          </div>
+
+          <div class="col s12 l3">
+            <div class="row" style="margin: 15px 0px!important;">
+              <div class="col s12">
+                <a class="btn btn-principal text-negrita" href="#" >Postula Aquí</a>
+              </div>
+            </div>
+            <div class="row" style="margin: 25px 0px!important;">
+              <h5 class="titulo-borde-izquierdo-2">DATOS GENERALES</h5>
+              <div class="row" style="margin: 12px 0px!important;">
+                  <div class="col s2 flex-row-start text-negrita">
+                    <i class="material-icons  color-secundario">edit</i>
+                  </div>
+                  <div class="col s10">
+                    <a href="#" class="black-text">Presentación</a>
+                  </div>
+              </div>
+              <div class="row" style="margin: 12px 0px!important;">
+                  <div class="col s2 flex-row-start text-negrita">
+                    <i class="material-icons  color-secundario">start</i>
+                  </div>
+                  <div class="col s10">
+                    <a href="#" class="black-text">Beneficios</a>
+                  </div>
+              </div>
+              <div class="row" style="margin: 12px 0px!important;">
+                  <div class="col s2 flex-row-start text-negrita">
+                    <i class="material-icons  color-secundario">account_balance</i>
+                  </div>
+                  <div class="col s10">
+                    <a href="#" class="black-text">IES Eleigles</a>
+                  </div>
+              </div>
+              <div class="row" style="margin: 12px 0px!important;">
+                  <div class="col s2 flex-row-start text-negrita">
+                    <i class="material-icons  color-secundario">content_paste</i>
+                  </div>
+                  <div class="col s10">
+                    <a href="#" class="black-text">Requermientos para Postular</a>
+                  </div>
+              </div>
+              <div class="row" style="margin: 12px 0px!important;">
+                  <div class="col s2 flex-row-start text-negrita">
+                    <i class="material-icons  color-secundario">bubble_chart</i>
+                  </div>
+                  <div class="col s10">
+                    <a href="#" class="black-text">Áreas de Conocimiento</a>
+                  </div>
+              </div>
+              <div class="row" style="margin: 12px 0px!important;">
+                  <div class="col s2 flex-row-start text-negrita">
+                    <i class="material-icons  color-secundario">linear_scale</i>
+                  </div>
+                  <div class="col s10">
+                    <a href="#" class="black-text">Pasos para Postular</a>
+                  </div>
+              </div>
+              <div class="row" style="margin: 12px 0px!important;">
+                  <div class="col s2 flex-row-start text-negrita">
+                    <i class="material-icons  color-secundario">date_range</i>
+                  </div>
+                  <div class="col s10">
+                    <a href="#" class="black-text">Cronograma</a>
+                  </div>
+              </div>
+              <div class="row" style="margin: 12px 0px!important;">
+                  <div class="col s2 flex-row-start text-negrita">
+                    <i class="material-icons  color-secundario">schedule</i>
+                  </div>
+                  <div class="col s10">
+                    <a href="#" class="black-text">Duración de la Beca</a>
+                  </div>
+              </div>
+              <div class="row" style="margin: 12px 0px!important;">
+                  <div class="col s2 flex-row-start text-negrita">
+                    <i class="material-icons  color-secundario">inbox</i>
+                  </div>
+                  <div class="col s10">
+                    <a href="#" class="black-text">Documentación Obligatorios</a>
+                  </div>
+              </div>
+              <div class="row" style="margin: 12px 0px!important;">
+                  <div class="col s2 flex-row-start text-negrita">
+                    <i class="material-icons  color-secundario">class</i>
+                  </div>
+                  <div class="col s10">
+                    <a href="#" class="black-text">Bases y Modificaciones</a>
+                  </div>
+              </div>
+            </div>
+            <div class="row" style="margin: 25px 0px!important;">
+              <h5 class="titulo-borde-izquierdo-2">DOCUMENTOS</h5>
+              <div class="col s12">
+                <ul class="">
+                  <li class="flex-row-start" style="margin:10px 0px!important"><i class="material-icons color-secundario" style="margin-right:5px!important">cloud_download</i><a href="#" class="black-text">Documento 1</a></li>
+                  <li class="flex-row-start" style="margin:10px 0px!important"><i class="material-icons color-secundario" style="margin-right:5px!important">cloud_download</i><a href="#" class="black-text">Documento 1</a></li>
+                  <li class="flex-row-start" style="margin:10px 0px!important"><i class="material-icons color-secundario" style="margin-right:5px!important">cloud_download</i><a href="#" class="black-text">Documento 1</a></li>
+
+                </ul>
+              </div>
+
+            </div>
+            <div class="row" style="margin: 25px 0px!important;">
+              <h5 class="titulo-borde-izquierdo-2">RESULTADOS</h5>
+              <div class="col s12">
+                <ul class="">
+                  <li class="flex-row-start" style="margin:10px 0px!important"><i class="material-icons color-secundario" style="margin-right:5px!important">cloud_download</i><a href="#" class="black-text">Resultados 1</a></li>
+                  <li class="flex-row-start" style="margin:10px 0px!important"><i class="material-icons color-secundario" style="margin-right:5px!important">cloud_download</i><a href="#" class="black-text">Resultados 1</a></li>
+                  <li class="flex-row-start" style="margin:10px 0px!important"><i class="material-icons color-secundario" style="margin-right:5px!important">cloud_download</i><a href="#" class="black-text">Resultados 1</a></li>
+
+                </ul>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+</div>
+<?php include("footer.php"); ?>
