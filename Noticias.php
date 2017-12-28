8fd72f2935f8617c12af623d876219b145e0656f
DANIELCOREI7\Daniel <daniel238035@hotmail.com>
2017-12-28 18:37:25 -0500

    Actualización Noticia 1

--- Noticias.php
+++ Noticias.php
@@ -1,143 +1,132 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Noticias</title>
-	<link rel="stylesheet" href="css/materialize.min.css">
-	<link rel="stylesheet" href="css/stylos.css">
-</head>
-<body>
-		<div class="row">
-			<div class="card-panel bg-secundario centered flex-row">
-				<h2 class="white-text">Noticias</h2>
+<?php include("header.php"); ?>
+
+<div class="cuerpo">
+	<div class="row">
+		<div class="card-panel bg-secundario centered flex-row">
+			<h2 class="white-text">Noticias</h2>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col s12 offset-l1 l7">
+			<div class="row">
+				<h4>Nombre de la Noticia</h4>
+			</div>
+			<div class="row" style="padding-bottom: 5px">
+				<div class="col s6">
+					<div>
+						<span class="icon-calendar"></span> Fecha: 18 de Agosto
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<img src="imagenes/FCF.jpg" class="responsive-img z-depth-1 imagen-evento">
+			</div>
+
+			<div class="row">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 			</div>
 		</div>
-
-		<di class="row">
-			<div class="col s12 offset-l1 l7">
-				<div class="row">
-					<h4>Nombre de la Noticia</h4>
+		<div class="col s12 l3">
+			<div class="row" style="padding-bottom: 25px">
+				<h5 class="bordes">
+					Ultimas Noticias
+				</h5>
+				<div class="col s12">
+					<h6>28 de noviembre</h6>
+					<h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat.</h6>
 				</div>
-				<div class="row" style="padding-bottom: 5px">
-					<div class="col s6">
-						<div> 
-							<span class="icon-calendar"></span> Fecha: 18 de Agosto
-						</div>
-					</div>
+				<div class="col s12">
+					<h6>28 de noviembre</h6>
+					<h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat.</h6>
 				</div>
-				<div class="row">
-					<img src="imagenes/FCF.jpg" class="responsive-img z-depth-1 imagen-evento">
-				</div>				
-
-				<div class="row">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				<div class="col s12">
+					<h6>28 de noviembre</h6>
+					<h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					consequat.</h6>
 				</div>
 			</div>
-			<div class="col s12 l3">
-				<div class="row" style="padding-bottom: 25px">
-					<h5 class="bordes">
-						Ultimas Noticias
-					</h5>
-					<div class="col s12">
-						<h6>28 de noviembre</h6>
-						<h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat.</h6>
+			<div class="row" >
+				<h5 class="bordes">
+					Ultimos Eventos
+				</h5>
+
+				<div class="col s12 espacioEvento">
+					<div class="col s4 bg-primario">
+						<p  style="text-align:center" class="white-text"><span style="font-size: 20px">18</span><br> Nov</p>
 					</div>
-					<div class="col s12">
-						<h6>28 de noviembre</h6>
-						<h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat.</h6>
-					</div>
-					<div class="col s12">
-						<h6>28 de noviembre</h6>
-						<h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat.</h6>
+					<div class="col s8">
+						Universidad Nacional Pedro Ruiz Gallo
 					</div>
 				</div>
-				<div class="row" >
-					<h5 class="bordes">
-						Ultimos Eventos
-					</h5>
 
-					<div class="col s12 espacioEvento">
-						<div class="col s4 bg-primario">
-							<p  style="text-align:center" class="white-text"><span style="font-size: 20px">18</span><br> Nov</p>
-						</div>
-						<div class="col s8">
-							Universidad Nacional Pedro Ruiz Gallo
-						</div>
+
+				<div class="col s12 espacioEvento">
+					<div class="col s4 bg-primario">
+						<p  style="text-align:center" class="white-text"><span style="font-size: 20px">18</span><br> Nov</p>
 					</div>
+					<div class="col s8">
+						Universidad Nacional Pedro Ruiz Gallo
 
-
-					<div class="col s12 espacioEvento">
-						<div class="col s4 bg-primario">
-							<p  style="text-align:center" class="white-text"><span style="font-size: 20px">18</span><br> Nov</p>
-						</div>
-						<div class="col s8">
-							Universidad Nacional Pedro Ruiz Gallo
-				
-						</div>
-					</div>
-					<div class="col s12 espacioEvento">
-						<div class="col s4 bg-primario">
-							<p  style="text-align:center" class="white-text"><span style="font-size: 20px">18</span><br> Nov</p>
-						</div>
-						<div class="col s8">
-							Universidad Nacional Pedro Ruiz Gallo
-						</div>
 					</div>
 				</div>
-				
-				
+				<div class="col s12 espacioEvento">
+					<div class="col s4 bg-primario">
+						<p  style="text-align:center" class="white-text"><span style="font-size: 20px">18</span><br> Nov</p>
+					</div>
+					<div class="col s8">
+						Universidad Nacional Pedro Ruiz Gallo
+					</div>
+				</div>
 			</div>
 
-		</di>
-		
 
+		</div>
 
+	</div>
+</div>
 
-		<!--adjuntando los archivos js-->
-		<script type="js/jquery.min.js"></script>
-		<script type="js/materialize.min.js"></script>
-</body>
-</html>
+<?php include("footer.php"); ?>
