d98dddfc7e21f9c158cd2161debb245671e063b5
DESKTOP-CT2KCEV\Anton <antonycarrasco24@gmail.com>
2017-12-29 17:29:32 -0500

    29 .5

--- listaotrasconv.php
+++ listaotrasconv.php
@@ -3,203 +3,198 @@
 
 
 <div class="cuerpo">
-		<div class="row" style="margin:0px!important;">
-			<div class="card-panel bg-secundario centered flex-row" style="margin:0px!important;">
-				<h2 class="white-text">Otras Convocatorias</h2>
-			</div>
-		</div>
-		<div class="container seccion">
-			<div class="row" style="margin-bottom: 10px!important" >
-				<div class="col s12 m6">
-					<div class="card" style="padding:0px!important;">
-						<div class="card-image waves-effect waves-block waves-light">
-							<img class="activator" src="images/portadas/portada3.jpg">
-						</div>
-						<div class="card-content" style="padding:15px!important;">
-							<div class="row flex-row">
-								<div class="col s10">
-									<h5 class="titulo-borde-izquierdo-2 activator">Nombre de la Convocatoria</h5>
-								</div>
-								<div class="col s2">
-									<span class="card-title activator grey-text text-darken-4"><i class="material-icons right">more_vert</i></span>
-								</div>
-							</div>
-							<div class="row">
-								<div class="col s12" style="margin: 8px 0px!important;">
-									<div class="row">
-											<div class="col s2 flex-row-start">
-												<i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Fecha">event</i>
-											</div>
-											<div class="col s10">
-												18 de agosto del 2017
-											</div>
-									</div>
-								</div>
-							</div>
-							<div class="row" style="padding: 10px !important">
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-							</div>
+  <div class="row portada-secundario portada-5 ">
+    <div class="portada-principal-oscuro flex seccion-2">
+        <div class="row">
+          <div class="col s12 l5 offset-l1">
+            <h2 class="white-text">OTRAS CONVOCATORIAS</h2>
+            <p class="white-text">Aquí podrás encontrar información sobre los programas nacionales e internacionales de formación e intercambios de carácter universitario. </p>
+          </div>
+        </div>
+    </div>
+  </div>
 
-						</div>
-						<div class="card-action" style="padding: 5px 5px !important">
-							<a class="btn btn-flat green-text text-negrita" href="http://localhost/practicas/movilidad.php" >Más</a>
-						</div>
-					</div>
-				</div>
+  <div class="row  white seccion">
+            <div class="container">
+              <div class="row">
+                <div class="col s12 m4">
+                  <div class="card" style="padding:0px!important;">
+                    <div class="card-image waves-effect waves-block waves-light">
+                      <img class="activator" src="images/portadas/portada3.jpg">
+                    </div>
+                    <div class="card-content" style="padding:15px!important;">
+                      <div class="row flex-row">
+                        <div class="col s10">
+                          <h5 class="titulo-borde-izquierdo-2 activator">Nombre de la Convocatoria</h5>
+                        </div>
+                        <div class="col s2">
+                          <span class="card-title activator grey-text text-darken-4"><i class="material-icons right">more_vert</i></span>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col s12" style="margin: 8px 0px!important;">
+                          <div class="row" >
+                              <div class="col s2 flex-row-start">
+                                <i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Institución">place</i>
+                              </div>
+                              <div class="col s10">
+                                Universidad Nacional Pedro Ruiz Gallo
+                              </div>
+                          </div>
+                        </div>
+                        <div class="col s12" style="margin: 8px 0px!important;">
+                          <div class="row">
+                              <div class="col s2 flex-row-start">
+                                <i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Inicio">event</i>
+                              </div>
+                              <div class="col s10">
+                                18 de agosto del 2017
+                              </div>
+                          </div>
+                        </div>
+                        <div class="col s12" style="margin: 8px 0px!important;">
+                          <div class="row">
+                              <div class="col s2 flex-row-start">
+                                <i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Duración">date_range</i>
+                              </div>
+                              <div class="col s10">
+                                3 años
+                              </div>
+                          </div>
+                        </div>
+                      </div>
 
-				<div class="col s12 m6">
-					<div class="card" style="padding:0px!important;">
-						<div class="card-image waves-effect waves-block waves-light">
-							<img class="activator" src="images/portadas/portada3.jpg">
-						</div>
-						<div class="card-content" style="padding:15px!important;">
-							<div class="row flex-row">
-								<div class="col s10">
-									<h5 class="titulo-borde-izquierdo-2 activator">Nombre de la Convocatoria</h5>
-								</div>
-								<div class="col s2">
-									<span class="card-title activator grey-text text-darken-4"><i class="material-icons right">more_vert</i></span>
-								</div>
-							</div>
-							<div class="row">
-								<div class="col s12" style="margin: 8px 0px!important;">
-									<div class="row">
-											<div class="col s2 flex-row-start">
-												<i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Fecha">event</i>
-											</div>
-											<div class="col s10">
-												18 de agosto del 2017
-											</div>
-									</div>
-								</div>
-							</div>
-							<div class="row" style="padding: 10px;!important">
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-							</div>
+                    </div>
+                    <div class="card-action" style="padding: 5px 5px!important">
+                      <a class="btn btn-flat green-text text-negrita" href="http://localhost/practicas/movilidad.php" >Inscríbete</a>
+                    </div>
+                    <div class="card-reveal">
+                      <span class="card-title grey-text text-darken-4">Nombre de la Convocatoria<i class="material-icons right">close</i></span>
+                      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                    </div>
+                  </div>
+                </div>
 
-						</div>
-						<div class="card-action" style="padding: 5px 5px!important">
-							<a class="btn btn-flat green-text text-negrita" href="http://localhost/practicas/movilidad.php" >Más</a>
-						</div>
-					</div>
-				</div>
-			</div>
+                <div class="col s12 m4">
+                  <div class="card" style="padding:0px!important;">
+                    <div class="card-image waves-effect waves-block waves-light">
+                      <img class="activator" src="images/portadas/portada3.jpg">
+                    </div>
+                    <div class="card-content" style="padding:15px!important;">
+                      <div class="row flex-row">
+                        <div class="col s10">
+                          <h5 class="titulo-borde-izquierdo-2 activator">Nombre de la Convocatoria</h5>
+                        </div>
+                        <div class="col s2">
+                          <span class="card-title activator grey-text text-darken-4"><i class="material-icons right">more_vert</i></span>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col s12" style="margin: 8px 0px!important;">
+                          <div class="row" >
+                              <div class="col s2 flex-row-start">
+                                <i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Institución">place</i>
+                              </div>
+                              <div class="col s10">
+                                Universidad Nacional Pedro Ruiz Gallo
+                              </div>
+                          </div>
+                        </div>
+                        <div class="col s12" style="margin: 8px 0px!important;">
+                          <div class="row">
+                              <div class="col s2 flex-row-start">
+                                <i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Inicio">event</i>
+                              </div>
+                              <div class="col s10">
+                                18 de agosto del 2017
+                              </div>
+                          </div>
+                        </div>
+                        <div class="col s12" style="margin: 8px 0px!important;">
+                          <div class="row">
+                              <div class="col s2 flex-row-start">
+                                <i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Duración">date_range</i>
+                              </div>
+                              <div class="col s10">
+                                3 años
+                              </div>
+                          </div>
+                        </div>
+                      </div>
 
-			<div class="row">
-				<div class="col s12 m4">
-					<div class="card" style="padding:0px!important;">
-						<div class="card-image waves-effect waves-block waves-light">
-							<img class="activator" src="images/portadas/portada3.jpg">
-						</div>
-						<div class="card-content" style="padding:15px!important;">
-							<div class="row flex-row">
-								<div class="col s10">
-									<h5 class="titulo-borde-izquierdo-2 activator">Nombre de la Convocatoria</h5>
-								</div>
-								<div class="col s2">
-									<span class="card-title activator grey-text text-darken-4"><i class="material-icons right">more_vert</i></span>
-								</div>
-							</div>
-							<div class="row">
-								<div class="col s12" style="margin: 8px 0px!important;">
-									<div class="row">
-											<div class="col s2 flex-row-start">
-												<i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Fecha">event</i>
-											</div>
-											<div class="col s10">
-												18 de agosto del 2017
-											</div>
-									</div>
-								</div>
-							</div>
+                    </div>
+                    <div class="card-action" style="padding: 5px 5px!important">
+                      <a class="btn btn-flat green-text text-negrita" href="http://localhost/practicas/movilidad.php" >Inscríbete</a>
+                    </div>
+                    <div class="card-reveal">
+                      <span class="card-title grey-text text-darken-4">Nombre de la Convocatoria<i class="material-icons right">close</i></span>
+                      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                    </div>
+                  </div>
+                </div>
 
-						</div>
-						<div class="card-action" style="padding: 5px 5px!important">
-							<a class="btn btn-flat green-text text-negrita" href="http://localhost/practicas/movilidad.php" >Más</a>
-						</div>
-						<div class="card-reveal">
-							<span class="card-title grey-text text-darken-4">Nombre de la Convocatoria<i class="material-icons right">close</i></span>
-							<p>Here is some more information about this product that is only revealed once clicked on.</p>
-						</div>
-					</div>
-				</div>
-				<div class="col s12 m4">
-					<div class="card" style="padding:0px!important;">
-						<div class="card-image waves-effect waves-block waves-light">
-							<img class="activator" src="images/portadas/portada3.jpg">
-						</div>
-						<div class="card-content" style="padding:15px!important;">
-							<div class="row flex-row">
-								<div class="col s10">
-									<h5 class="titulo-borde-izquierdo-2 activator">Nombre de la Convocatoria</h5>
-								</div>
-								<div class="col s2">
-									<span class="card-title activator grey-text text-darken-4"><i class="material-icons right">more_vert</i></span>
-								</div>
-							</div>
-							<div class="row">
-								<div class="col s12" style="margin: 8px 0px!important;">
-									<div class="row">
-											<div class="col s2 flex-row-start">
-												<i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Fecha">event</i>
-											</div>
-											<div class="col s10">
-												18 de agosto del 2017
-											</div>
-									</div>
-								</div>
-							</div>
+                <div class="col s12 m4">
+                  <div class="card" style="padding:0px!important;">
+                    <div class="card-image waves-effect waves-block waves-light">
+                      <img class="activator" src="images/portadas/portada3.jpg">
+                    </div>
+                    <div class="card-content" style="padding:15px!important;">
+                      <div class="row flex-row">
+                        <div class="col s10">
+                          <h5 class="titulo-borde-izquierdo-2 activator">Nombre de la Convocatoria</h5>
+                        </div>
+                        <div class="col s2">
+                          <span class="card-title activator grey-text text-darken-4"><i class="material-icons right">more_vert</i></span>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col s12" style="margin: 8px 0px!important;">
+                          <div class="row" >
+                              <div class="col s2 flex-row-start">
+                                <i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Institución">place</i>
+                              </div>
+                              <div class="col s10">
+                                Universidad Nacional Pedro Ruiz Gallo
+                              </div>
+                          </div>
+                        </div>
+                        <div class="col s12" style="margin: 8px 0px!important;">
+                          <div class="row">
+                              <div class="col s2 flex-row-start">
+                                <i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Inicio">event</i>
+                              </div>
+                              <div class="col s10">
+                                18 de agosto del 2017
+                              </div>
+                          </div>
+                        </div>
+                        <div class="col s12" style="margin: 8px 0px!important;">
+                          <div class="row">
+                              <div class="col s2 flex-row-start">
+                                <i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Duración">date_range</i>
+                              </div>
+                              <div class="col s10">
+                                3 años
+                              </div>
+                          </div>
+                        </div>
+                      </div>
 
-						</div>
-						<div class="card-action" style="padding: 5px 5px!important">
-							<a class="btn btn-flat green-text text-negrita" href="http://localhost/practicas/movilidad.php" >Más</a>
-						</div>
-						<div class="card-reveal">
-							<span class="card-title grey-text text-darken-4">Nombre de la Convocatoria<i class="material-icons right">close</i></span>
-							<p>Here is some more information about this product that is only revealed once clicked on.</p>
-						</div>
-					</div>
-				</div>
-				<div class="col s12 m4">
-					<div class="card" style="padding:0px!important;">
-						<div class="card-image waves-effect waves-block waves-light">
-							<img class="activator" src="images/portadas/portada3.jpg">
-						</div>
-						<div class="card-content" style="padding:15px!important;">
-							<div class="row flex-row">
-								<div class="col s10">
-									<h5 class="titulo-borde-izquierdo-2 activator">Nombre de la Convocatoria</h5>
-								</div>
-								<div class="col s2">
-									<span class="card-title activator grey-text text-darken-4"><i class="material-icons right">more_vert</i></span>
-								</div>
-							</div>
-							<div class="row">
-								<div class="col s12" style="margin: 8px 0px!important;">
-									<div class="row">
-											<div class="col s2 flex-row-start">
-												<i class="material-icons tooltipped color-secundario" data-position="left" data-delay="50" data-tooltip="Fecha">event</i>
-											</div>
-											<div class="col s10">
-												18 de agosto del 2017
-											</div>
-									</div>
-								</div>
-							</div>
+                    </div>
+                    <div class="card-action" style="padding: 5px 5px!important">
+                      <a class="btn btn-flat green-text text-negrita" href="http://localhost/practicas/movilidad.php" >Inscríbete</a>
+                    </div>
+                    <div class="card-reveal">
+                      <span class="card-title grey-text text-darken-4">Nombre de la Convocatoria<i class="material-icons right">close</i></span>
+                      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                    </div>
+                  </div>
+                </div>
 
-						</div>
-						<div class="card-action" style="padding: 5px 5px!important">
-							<a class="btn btn-flat green-text text-negrita" href="http://localhost/practicas/movilidad.php" >Más</a>
-						</div>
-						<div class="card-reveal">
-							<span class="card-title grey-text text-darken-4">Nombre de la Convocatoria<i class="material-icons right">close</i></span>
-							<p>Here is some more information about this product that is only revealed once clicked on.</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+              </div>
+            </div>
+  </div>
+</div>
 
 
 
